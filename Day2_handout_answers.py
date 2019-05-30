e7f9d08f53e519048f31469455a80b8d6e571bc9
Yadira <nvo.yadigaibor@gmail.com>
2019-05-30 18:17:13 -0500

    05/30/19

---
+++ Day2_handout_answers.py
@@ -0,0 +1,23 @@
+'''
+Yadira Gaibor
+Handout answers
+'''
+>1:
+    The smaller the tolerance, the more iterations it will have to go through.
+    10e-3: 7 iterations
+    10e-6: 17 iters.
+    10e-9: 27 iters.
+>2:
+    You can make bysection.py a function itself. So that you can call it from other programs.
+    Then you would pass tolerance and the function as inputs.
+>3:
+    Tuples are immutabale and cant have repeated elements.
+    Lists can be altered or appended.
+    Dictionaries are set up in pairs (keys and corresponding values), they are mutable.
+    Sets are not set up in pairs, and they contain distinct objects.
+>4:
+    You can do something like:
+        a=[0,1,'w']
+        if type(a) is list:
+            print("yes, a list")
+    
